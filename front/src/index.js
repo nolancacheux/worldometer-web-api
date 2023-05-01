51ef63dd30b15a99685bff5d895711a7f9b8e335
ThibautLemichel <thibaut.lemichel@student.junia.com>
2023-05-01 11:44:52 +0200

    correctif

--- front/src/index.js
+++ front/src/index.js
@@ -84,25 +84,17 @@ class MultiSeriesPieChart extends React.Component {
                     legend: {
                         labels: {
                             generateLabels: function (chart) {
-                                // Get the default label list
                                 const original = Chart.overrides.pie.plugins.legend.labels.generateLabels;
                                 const labelsOriginal = original.call(this, chart);
 
-                                // Build an array of colors used in the datasets of the chart
                                 let datasetColors = chart.data.datasets.map(function (e) {
                                     return e.backgroundColor;
                                 });
                                 datasetColors = datasetColors.flat();
 
-                                // Modify the color and hide state of each label
                                 labelsOriginal.forEach(label => {
-                                    // There are twice as many labels as there are datasets. This converts the label index into the corresponding dataset index
                                     label.datasetIndex = (label.index - label.index % 2) / 2;
-
-                                    // The hidden state must match the dataset's hidden state
                                     label.hidden = !chart.isDatasetVisible(label.datasetIndex);
-
-                                    // Change the color to match the dataset
                                     label.fillStyle = datasetColors[label.index];
                                 });
 
